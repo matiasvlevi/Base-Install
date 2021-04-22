e7d9134b6ca27243111c95a2be8cacfe5d8ba7fe
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-04-22 00:56:30 -0400

    Create ubuntudesktop.sh

---
+++ ubuntudesktop.sh
@@ -0,0 +1,13 @@
+
+# Update
+sudo apt-get update
+
+# Install atom
+sudo add-apt-repository ppa:webup8team/atom
+sudo apt-get install atom
+
+# Node & npm
+sudo apt-get install nodejs
+
+# Git 
+sudo apt-get install git
