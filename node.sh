d134a2598f55d6d542c0e05deeb08e9d3c361fca
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-07-30 09:41:44 -0400

    node v10.16.3

--- node.sh
+++ node.sh
@@ -2,12 +2,12 @@
 if ! type "node" > /dev/null; then
     echo "Installing Node.js v9.7.1 (armv6l)" ;
     # Installing from armv6l distributions.
-    curl -o node-v9.7.1-linux-armv6l.tar.gz https://nodejs.org/dist/v9.7.1/node-v9.7.1-linux-armv6l.tar.gz ;
-    tar -xzf node-v9.7.1-linux-armv6l.tar.gz ;
-    sudo cp -r node-v9.7.1-linux-armv6l/* /usr/local/;
+    curl -o node-v10.16.3-linux-armv6l.tar.gz https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-armv6l.tar.xz ;
+    tar -xzf node-v10.16.3-linux-armv6l.tar.xz ;
+    sudo cp -r node-v10.16.3-linux-armv6l/* /usr/local/;
     # Deleting tarballs.
-    sudo rm -r node-v9.7.1-linux-armv6l ;
-    sudo rm node-v9.7.1-linux-armv6l.tar.gz ;
+    sudo rm -r node-v10.16.3-linux-armv6l ;
+    sudo rm node-v10.16.3-linux-armv6l.tar.gz ;
     echo "Removed tarballs." ;
     echo ;
     echo ;
