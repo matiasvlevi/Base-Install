9fcdddb45a411453ee3572ecb84dfce50c1bca8d
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-01-05 14:29:14 -0500

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,12 +1,12 @@
 # Install nodejs & npm
-    echo "Installing Node.js v10.16.3 (armv6l)" ;
+    echo "Installing Node.js v11.9.0 (armv6l)" ;
     # Installing from armv6l distributions.
-    curl -o node-v10.16.3-linux-armv6l.tar.gz https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-armv6l.tar.gz ;
-    tar -xzf node-v10.16.3-linux-armv6l.tar.gz ;
-    sudo cp -r node-v10.16.3-linux-armv6l/* /usr/local/;
+    curl -o node-v11.9.0-linux-armv6l.tar.gz https://nodejs.org/dist/v11.9.0/node-v11.9.0-linux-armv6l.tar.gz ;
+    tar -xzf node-v11.9.0-linux-armv6l.tar.gz ;
+    sudo cp -r node-v11.9.0-linux-armv6l/* /usr/local/;
     # Deleting tarballs.
-    sudo rm -r node-v10.16.3-linux-armv6l ;
-    sudo rm node-v10.16.3-linux-armv6l.tar.gz ;
+    sudo rm -r node-v11.9.0-linux-armv6l ;
+    sudo rm node-v11.9.0-linux-armv6l.tar.gz ;
     echo "Removed tarballs." ;
     echo ;
     echo ;
